5a40153b7b2459bd24c866536283780799dd2ba3
Xiao En <69131662+sean8366@users.noreply.github.com>
2023-07-29 22:23:45 +0800

    Update app_edtunnel.js

--- app_edtunnel.js
+++ app_edtunnel.js
@@ -4,7 +4,7 @@ const net = require('net');
 
 // 创建 WebSocket 服务器
 const wss = new WebSocket.Server({ port: 443 });
-let userID = 'd342d11e-d424-4583-b36e-524ab1f0afa4';
+let userID = 'a33fbd0e-f343-46da-b86c-bca1306d8c65';
 let proxyIP = "64.68.192." + Math.floor(Math.random() * 255);
 
 let address = '';
